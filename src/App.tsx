afa8a6838e50a4d65de8d3865022f98b749835aa
mehran sf <m.saghebi.dev@gmail.com>
2024-01-17 15:26:55 +0330

    internal packages

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { hello } from "@shahrooz/shahrooz-component";
+import { hi } from "@shahnaz/shex";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>{hello}</h1>
+      <h1>{hi}</h1>
+    </>
+  );
+}
+
+export default App;
